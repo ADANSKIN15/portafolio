35004001d8a44535ab57cc95184fc614553206d3
ADANSKIN15 <adansking22@gmail.com>
2024-06-18 21:25:09 -0500

    subiendo progrmas

---
+++ programa#1.py
@@ -0,0 +1,16 @@
+# Escribe un programa que solicite al usuario un número y determine si es positivo, 
+# negativo o cero
+
+print('positivo , negativo o cero')
+print('**************************')
+
+numero = float(input('ingrese el valor: \n'))
+
+if numero >0:
+    print('-> el numero es positivo \n')
+    
+else:
+    print('-> el numero es negativo \n')
+  
+  
+print('"FIN"')
